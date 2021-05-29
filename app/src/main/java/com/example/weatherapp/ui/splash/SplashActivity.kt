9401408e752e2ac9e494167f8aa61c71266c1456
heba_alhendi <habdullah@nafit.com>
2021-05-29 17:53:45 +0300

    - launcher icon added - hide navigation bar BuildSDK < 30 added

--- app/src/main/java/com/example/weatherapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.WindowManager
 import com.example.weatherapp.databinding.ActivitySplashBinding
 import com.example.weatherapp.ui.base.BaseActivity
@@ -34,8 +35,13 @@ class SplashActivity : BaseActivity() {
         } else {
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             )
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+
         }
     }
 }
